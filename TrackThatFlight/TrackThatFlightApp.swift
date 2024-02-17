21bd0a579b619be5412e14f95d2982c3bff32e8a
Kapil Shanbhag <shanbhag.kapil09@gmail.com>
2024-02-17 13:45:55 +0530

    Initial Commit

---
+++ TrackThatFlight/TrackThatFlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackThatFlightApp.swift
+//  TrackThatFlight
+//
+//  Created by Kapil Shanbhag on 17/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TrackThatFlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
